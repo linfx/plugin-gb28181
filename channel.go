635b8cf47ce86f33c9a94331506ed858be60c7a0
sky <410728115@qq.com>
2024-06-25 14:16:42 +0800

    删除model.go

--- channel.go
+++ channel.go
@@ -145,6 +145,25 @@ func (c *Channel) MarshalJSON() ([]byte, error) {
 	return json.Marshal(m)
 }
 
+// Record 录像
+type Record struct {
+	DeviceID  string
+	Name      string
+	FilePath  string
+	Address   string
+	StartTime string
+	EndTime   string
+	Secrecy   int
+	Type      string
+}
+
+// 预置位
+type Preset struct {
+	DeviceID   string // 设备ID
+	PresetID   int    // 预置位编号
+	PresetName string // 预置位名称
+}
+
 // Channel 通道
 type ChannelInfo struct {
 	DeviceID     string // 通道ID
